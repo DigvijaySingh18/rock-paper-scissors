eaf5aad2f3e5c908871d2da1552d26ce63463232
DigvijaySingh18 <digvijaysingh1898@gmail.com>
2021-07-15 08:16:39 +0530

    Added UI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 
 // Make a function computerPlay()
 function computerPlay(){
@@ -18,50 +21,70 @@ function playRound(playerSelection, computerSelection){
     if ((playerSelection == "rock" && computerSelection == "scissors") 
         || (playerSelection == "paper" && computerSelection == "rock")
         || (playerSelection == "scissors" && computerSelection == "paper")){
+            playerScore += 1;
             roundResult = `You Win, ${playerSelection} beats ${computerSelection}.`;
     } else if ((computerSelection == "rock" && playerSelection == "scissors")
         || (computerSelection == "paper" && playerSelection == "rock")
         || (computerSelection == "scissors" && playerSelection == "paper")){
+            computerScore += 1;
             roundResult = `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection == computerSelection){
         roundResult = "It's a Tie.";
     } else {
         roundResult = "Invalid Selection";
     }
-    // Return result
-    return roundResult;
-}
+    
+    // Store round information
+    displayDiv.textContent = roundResult;
+    scoreDiv.textContent = `Current Score is Player ${playerScore} : Computer ${computerScore}`;
 
-// Make a function game()
-function game(){
-    // Call playRound() function 5 times and track scores for each round
-    let roundOutcome;
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i=0; i<5; i++){
-        // Get user Input for playerSelection
-        let playerChoice = window.prompt("Enter your choice: Rock, Paper or Scissors");
-        // Set computerSelection equal to computerPlay
-        let computerChoice = computerPlay();
-        roundOutcome = playRound(playerChoice, computerChoice);
-        console.log(roundOutcome);
-        if (roundOutcome.search("Win") > 0){
-            playerScore += 1;
-        } else if (roundOutcome.search("Lose") > 0){
-            computerScore += 1;
+    // Check for Winner
+    if (playerScore == 5 || computerScore == 5){
+        // Remove play options
+        rock.parentElement.removeChild(rock);
+        paper.parentElement.removeChild(paper);
+        scissors.parentElement.removeChild(scissors);
+
+        // Display Game winner
+        if (playerScore == 5){
+            winnerDiv.textContent = `You Win the Game.\nFinal Score is Player ${playerScore} : ${computerScore} Computer.`;
+        } else if (computerScore == 5){
+            winnerDiv.textContent = `You Lose the Game.\nFinal Score is Player ${playerScore} : ${computerScore} Computer.`;
         }
-        console.log(`Current Score is Player ${playerScore} : Computer ${computerScore}`);
+        
+        // Choice to restart the Game
+        let delayinMilliseconds = 1000;
+        
+        setTimeout(function() {
+            let playAgain = confirm("Do you want to play again?");
+        if (playAgain){
+            window.location.reload();
+        }
+        }, delayinMilliseconds);
+        
     }
-
-    // Display winner
-    if (playerScore > computerScore){
-        console.log(`You Win the Game.\nFinal Score is Player ${playerScore} : ${computerScore} Computer.`);
-    } else if (computerScore > playerScore){
-        console.log(`You Lose the Game.\nFinal Score is Player ${playerScore} : ${computerScore} Computer.`);
-    } else {
-        console.log(`The Game ended as Tie.\nFinal Score is Player ${playerScore} : ${computerScore} Computer.`);
-    }
+    
 }
 
-// Call game() function
-game();
+
+const container = document.querySelector("#container");
+const displayDiv = document.createElement("div");
+const scoreDiv = document.createElement("div");
+const winnerDiv = document.createElement("div");
+
+
+container.appendChild(displayDiv);
+
+container.appendChild(scoreDiv);
+
+container.appendChild(winnerDiv);
+
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+
+rock.addEventListener("click", () => playRound("Rock", computerPlay()));
+paper.addEventListener("click", () => playRound("Paper", computerPlay()));
+scissors.addEventListener("click",() => playRound("Scissors", computerPlay()));
